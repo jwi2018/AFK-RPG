6ac716fdba4aa90e0dcc643a1f47332d4d4ab247
digdagdac <kimhs0459@naver.com>
2023-03-25 16:59:51 +0900

    add-spawn

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] private float maxHp = 100f;
+    [SerializeField] private float disappearTime = 1f;
     Animator animator;
     public Transform player;
+    public float Hp;
+    public float Damage;
     public float speed;
     public float atkCooltime = 4;
     public float atkDelay;
@@ -15,13 +20,24 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Hp = maxHp;
         animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+    public void TakeDamage(float damage)
+    {
+        Hp -= damage;
+        if (Hp <= 0f)
+        {
+            StartCoroutine(Disappear());
+        }
+    }
+
+
     private void Update()
     {
-         if(atkDelay >= 0)
+        if (atkDelay >= 0)
         {
             atkDelay -= Time.deltaTime;
         }
@@ -55,13 +71,23 @@ public class Enemy : MonoBehaviour
         }
 
         Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(boxpos.position, boxSize, 0);
-        foreach(Collider2D collider in collider2Ds)
+        foreach (Collider2D collider in collider2Ds)
         {
-            if(collider.tag == "Player")
+            if (collider.tag == "Player")
             {
-                Debug.Log("Damage");
+
+                Debug.Log(Damage);
             }
         }
+
+
+    }
+
+    private IEnumerator Disappear()
+    {
+        //animator.SetTrigger("Die"); // "Die" 애니메이션 재생 die애니메이션 미설정
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length); // 애니메이션 길이만큼 대기
+        Destroy(gameObject, disappearTime); // 일정 시간 후에 몬스터 오브젝트 제거
     }
 
 
